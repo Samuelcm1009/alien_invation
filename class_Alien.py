0305b4e161751e9a350abdb705e329ac60fff56c
Samuelcm1009 <samuelcm@sjtu.edu.cn>
2025-08-05 15:45:23 +0800

    test

---
+++ class_Alien.py
@@ -0,0 +1,22 @@
+class Alien:
+    '''The class to describe the aliens in the game'''
+    def __init__(self,color,point,speed,position):
+        self.color =  color
+        self.point = point
+        self.speed = speed
+        self.position = position
+    
+    def move_alien(self):
+        
+    def create_alien(self):
+    
+
+    def destory_alien(selfn):
+
+
+
+
+class GreenAlien(Alien):
+    def __init__(self,color='green',point=5,speed='slow',position):
+        super().__init__(color,point,speed,position)
+    
